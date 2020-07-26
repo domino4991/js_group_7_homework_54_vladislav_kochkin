89e7474e2da7a36267fe0b904bcab0aa0a5ab162
Vladislav Kochkin <ltrickfaim@gmail.com>
2020-07-26 14:46:57 +0600

    Change GameDeck

--- src/component/GameDeck/GameDeck.js
+++ src/component/GameDeck/GameDeck.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import './GameDeck.css';
+import GameDeckCell from "../GameDeckCell/GameDeckCell";
 
 const GameDeck = props => {
-    return (
-        <div className='game-deck'>
+    let deckClasses = ['game-deck'];
 
+    if(props.endGame) {
+        deckClasses.push('end-game');
+    }
+
+    return (
+        <div className={deckClasses.join(' ')}>
+            {props.cells.map(item => <GameDeckCell
+                key={item.id}
+                clicked={item.clicked}
+                ring={item.hasRing}
+                onCellClick={() => props.onCellClick(item.id)}
+            />)}
         </div>
     );
 };
